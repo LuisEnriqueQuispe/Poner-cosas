cf34f57aa30ddcd15f4eb517ab028280ad95e57e
Luis Enrique Quispe Alvarez <102596087+LuisEnriqueQuispe@users.noreply.github.com>
2022-10-06 08:49:36 -0500

    Add files via upload

---
+++ TestElevador/src/testelevador/Edificio.java
@@ -0,0 +1,15 @@
+package testelevador;
+
+public class Edificio {
+
+    private int NPiso;
+
+    public int getNPiso() {
+        return NPiso;
+    }
+
+    public void setNPiso(int NPiso) {
+        this.NPiso = NPiso;
+    }
+
+}
